9edc612feef0a67b63d911c2806e13407f1fbdde
Education-cite <tusarjobs@gmail.com>
2023-04-14 19:08:44 +0600

    s

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home:Scaffold(
-          body:Row(
-            children: [
-              Container(
-                height: double.infinity,
-                width: 100,
-                color: Colors.amber,
-              ),
-               Container(
-                height: 200.0,
-                width: double.infinity,
-                color: Colors.green,
-              ),
-               Container(
-                height: 200.0,
-                width: double.infinity,
-                color: Colors.pink,
-              ),
-          ],
+          body:Container(
+            child: ElevatedButton(onPressed: (){
+              print("click button");
+            },
+             child: Text("Click me"),
+            ),
           ),
-        ) ,
+          
+        
+    ),
     );
   }
 }
