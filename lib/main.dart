b2ab1a7b5444e2d190826733d6439e7883eb34e3
Education-cite <tusarjobs@gmail.com>
2023-04-19 11:59:55 +0600

    n

--- lib/main.dart
+++ lib/main.dart
@@ -11,43 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text("My First App"),
-            leading: Icon(Icons.search),
-            actions: [
-              Icon(Icons.alarm),
-              Icon(Icons.add),
-            ],
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              print("hellow i am click");
-            },
-            child: Icon(Icons.add),
-          ),
-          body: Container(
-            width: double.infinity,
-            height: double.infinity,
-            child: ListView(
-              children: [
-                listItem(color:Colors.green,text:"Nayem"),
-                listItem(color:Colors.amber,text:"tawhide"),
-                listItem(color:Colors.pink,text:"tusi"),
-                listItem(color:Colors.red,text:"pinki"),
-                listItem(color:Colors.blue,text:"Enamul"),
-                listItem(color:Colors.black,text:"Enamul"),
-                listItem(color:Colors.yellow,text:"Enamul"),
-                listItem(color:Colors.green,text:"Enamul"),
-                listItem(color:Colors.amber,text:"Enamul"),
-                listItem(color:Colors.pink,text:"Enamul"),
-                listItem(color:Colors.red,text:"Enamul"),
-                listItem(color:Colors.blue,text:"Enamul"),
-              
-              ],
+      home: SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: Text("Hellow bangladesh",
+            style: TextStyle(
+              color: Colors.red,
             ),
-          )),
+            ),
+            ),
+        )
+      ),
     );
   }
 }
