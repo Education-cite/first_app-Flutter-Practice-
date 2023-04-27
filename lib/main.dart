bc354d0d50098a42856af5bb61c1d8f18b1c4533
Education-cite <tusarjobs@gmail.com>
2023-04-28 01:39:55 +0600

    d

--- lib/main.dart
+++ lib/main.dart
@@ -163,48 +163,181 @@ class _FromPageState extends State<FromPage> {
 
 TextEditingController textEditingController = TextEditingController();
 
+List<String> image = [
+  "https://w7.pngwing.com/pngs/667/326/png-transparent-red-rose-rose-rose-corner-s-love-flower-arranging-presentation-thumbnail.png",
+    "https://w7.pngwing.com/pngs/667/326/png-transparent-red-rose-rose-rose-corner-s-love-flower-arranging-presentation-thumbnail.png",
+  "https://w7.pngwing.com/pngs/667/326/png-transparent-red-rose-rose-rose-corner-s-love-flower-arranging-presentation-thumbnail.png",
+  "https://w7.pngwing.com/pngs/667/326/png-transparent-red-rose-rose-rose-corner-s-love-flower-arranging-presentation-thumbnail.png"
 
+];
+var dropDownItemValue = 0;
+List<Color> color =[Colors.pink,Colors.red,Colors.amber,Colors.green];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("FromPage"),),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-              TextField(
-                controller: textEditingController,
-                cursorColor: Colors.red,
-                
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
-                  fillColor: Colors.black12,
-                  filled: true,
-                  hintText: "Enter your name",
-                  hintStyle: TextStyle(color: Colors.orange),
-                  labelText: "First Name",
-                  labelStyle: TextStyle(color: Colors.pink),
-                 // border: InputBorder.none, 
-                 border:OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10)
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.red),
-                  ),
-                  enabledBorder:  OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.red),
-                  ),
-                 prefixIcon: Icon(Icons.person),
-                 suffixIcon: Icon(Icons.arrow_upward),
-                ),
-              )
-      
-           ],     
+       body: GridView.builder(
+        itemCount: 4,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           
-           ),
-      ),
+          crossAxisCount:2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+           ), 
+       
+       itemBuilder: (context,index){
+        return Image.network(image[index]);
+        
+       }
+       )
+       
+       
+      //  GridView.count(
+      //   crossAxisCount: 3,
+      //   crossAxisSpacing: 10,
+      //   mainAxisSpacing: 10,
+      //   padding: EdgeInsets.all(8.0),
+      //  children: [
+      //   Container(
+      //     height: 200,
+      //     width: 200,
+      //     color: Colors.blue,
+      //   ),
+      //    Container(
+      //     height: 200,
+      //     width: 200,
+      //     color: Colors.amber,
+      //   ),
+      //    Container(
+      //     height: 200,
+      //     width: 200,
+      //     color: Colors.pink,
+      //   ),
+      //    Container(
+      //     height: 200,
+      //     width: 200,
+      //     color: Color.fromARGB(255, 152, 40, 66),
+      //   ),
+      //    Container(
+      //     height: 200,
+      //     width: 200,
+      //     color: Colors.green,
+      //   ),
+      //    Container(
+      //     height: 200,
+      //     width: 200,
+      //     color: Colors.red,
+      //   )
+      //  ],
+      //  ),
+
+
+      //     Column(
+      //   children: [
+      //     DropdownButton(
+      //       value: dropDownItemValue,
+      //       underline: Container(),
+      //       items: [
+      //         DropdownMenuItem(child: Text("item 1"),value: 0,),
+      //          DropdownMenuItem(child: Text("item 2"),value: 1,),
+      //           DropdownMenuItem(child: Text("item 3"),value: 2,),
+      //            DropdownMenuItem(child: Text("item 4"),value: 3,),
+      //       ],
+   
+      //     onChanged: (dynamic value){
+      //         setState(() {
+      //           dropDownItemValue = value;
+      //         });
+      //       print(dropDownItemValue);
+
+      //     },
+          
+      //     )
+      //   ],
+      //  ),
+
+       
+      //  ListView.separated(
+      //   separatorBuilder: (context, index) {
+      //      return   Divider(
+      //          thickness: 10,
+      //         color:Colors.white,
+      //        );
+      //   },
+      //   itemCount: 4,
+      //   itemBuilder:(context,index){
+      //     return   Container(
+      //          height: 150,
+      //         color:color[index],
+      //        );
+      //  }
+      //  ),
+      
+      //body: SingleChildScrollView(
+      //   scrollDirection: Axis.horizontal,
+      //   child: Row(
+      //     children: [
+      //         // TextField(
+      //         //   controller: textEditingController,
+      //         //   cursorColor: Colors.red,
+                
+      //         //   decoration: InputDecoration(
+      //         //     contentPadding: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+      //         //     fillColor: Colors.black12,
+      //         //     filled: true,
+      //         //     hintText: "Enter your name",
+      //         //     hintStyle: TextStyle(color: Colors.orange),
+      //         //     labelText: "First Name",
+      //         //     labelStyle: TextStyle(color: Colors.pink),
+      //         //    // border: InputBorder.none, 
+      //         //    border:OutlineInputBorder(
+      //         //       borderRadius: BorderRadius.circular(10)
+      //         //     ),
+      //         //     focusedBorder: OutlineInputBorder(
+      //         //       borderRadius: BorderRadius.circular(10),
+      //         //       borderSide: BorderSide(color: Colors.red),
+      //         //     ),
+      //         //     enabledBorder:  OutlineInputBorder(
+      //         //       borderRadius: BorderRadius.circular(10),
+      //         //       borderSide: BorderSide(color: Colors.red),
+      //         //     ),
+      //         //    prefixIcon: Icon(Icons.person),
+      //         //    suffixIcon: Icon(Icons.arrow_upward),
+      //         //   ),
+      //         // ),
+            
+      //        Container(
+      //          width: 150,
+      //         color: Colors.red,
+      //        ),
+      
+      //         Container(
+      //          width: 150,
+      //         color: Colors.pink,
+      //        ),
+      
+      //         Container(
+      //          width: 150,
+      //         color: Color.fromARGB(255, 19, 18, 18),
+      //        ),
+      
+      //         Container(
+      //          width: 150,
+      //         color: Color.fromARGB(255, 76, 54, 244),
+      //        ),
+      //         Container(
+      //          width: 150,
+      //         color: Colors.amber,
+      //        ),
+      //         Container(
+      //          width: 150,
+      //         color: Colors.green,
+      //        ),
+      
+      //      ],     
+          
+      //      ),
+      // ),
     );
   }
 }
